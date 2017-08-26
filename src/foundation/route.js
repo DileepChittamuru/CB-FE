e78e7acfcf36d9e0176a2f8e1e83ec08aaddd1bb
DileepChittamuru <chdileep8500@gmail.com>
2017-08-26 07:17:50 +0530

    CB-03 resolved the bug fix ie controller loading twice

--- src/foundation/route.js
+++ src/foundation/route.js
@@ -10,18 +10,15 @@
 		$stateProvider
 		.state("home", {
 			url:'/home',
-			templateUrl:"src/widgets/home/home.html",
-			controller: 'homeController'
+			templateUrl:"src/widgets/home/home.html"
 		})
 		.state("login", {
 			url:'/login',
-			templateUrl:'src/widgets/login/login.html',
-			controller: 'loginController'
+			templateUrl:'src/widgets/login/login.html'
 		})
 		.state("signUp", {
 			url:'/signUp',
-			templateUrl:'src/widgets/sign-up/sign-up.html',
-			controller: 'signUpController'
+			templateUrl:'src/widgets/sign-up/sign-up.html'
 		});
 	});
 }());
