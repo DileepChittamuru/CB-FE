ad7111c97a1d4e1f31aa69567c9746a811ca0145
DileepChittamuru <chdileep8500@gmail.com>
2017-09-10 10:55:16 +0530

    Add files via upload

--- E2E/home.js
+++ E2E/home.js
@@ -1,1 +1,7 @@
-
+describe('Testing home page', function() {
+ it('name should be home', function() {
+ browser.get('http://127.0.0.1:62424/index.html#/home');
+   var text = element(by.id('home'));
+   expect(text.getText()).toEqual("home");
+  });
+});
